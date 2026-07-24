b8e7810abcc544ddcd4f54006d7ce83bde4326f1
maggogerka <71784665+maggogerka@users.noreply.github.com>
2026-07-25 00:13:03 +0300

    test: cover v0.2 core behavior

--- test/test_core_logic/test_main.cpp
+++ test/test_core_logic/test_main.cpp
@@ -33,6 +33,37 @@ void test_retry_backoff_is_bounded() {
   TEST_ASSERT_EQUAL_UINT32(900000, core::retryDelay(20, 15000, 900000));
 }
 
+void test_pagination_stops_and_respects_limit() {
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextPage(1, 0, 100, 0, 200));
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextPage(1, 1, 100, 1, 200));
+  TEST_ASSERT_EQUAL_UINT32(2, core::nextPage(1, 100, 100, 100, 200));
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextPage(2, 100, 100, 200, 200));
+}
+
+void test_carousel_wraps_in_both_directions() {
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextIndex(0, 0));
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextIndex(0, 1));
+  TEST_ASSERT_EQUAL_UINT32(0, core::nextIndex(1, 2));
+  TEST_ASSERT_EQUAL_UINT32(1, core::previousIndex(0, 2));
+  TEST_ASSERT_EQUAL_UINT32(0, core::previousIndex(0, 0));
+}
+
+void test_touch_mapping_clamps_and_inverts() {
+  TEST_ASSERT_EQUAL_INT16(0, core::mapTouch(100, 100, 1100, 320, false));
+  TEST_ASSERT_EQUAL_INT16(319,
+                          core::mapTouch(1100, 100, 1100, 320, false));
+  TEST_ASSERT_EQUAL_INT16(319,
+                          core::mapTouch(100, 100, 1100, 320, true));
+  TEST_ASSERT_EQUAL_INT16(0,
+                          core::mapTouch(1100, 100, 1100, 320, true));
+}
+
+void test_cooldown_handles_millis_rollover() {
+  TEST_ASSERT_FALSE(core::cooldownReady(1200, 1000, 500));
+  TEST_ASSERT_TRUE(core::cooldownReady(1500, 1000, 500));
+  TEST_ASSERT_TRUE(core::cooldownReady(20, UINT32_MAX - 100, 100));
+}
+
 void runAllTests() {
   UNITY_BEGIN();
   RUN_TEST(test_truncates_long_text_without_splitting_utf8);
@@ -40,6 +71,10 @@ void runAllTests() {
   RUN_TEST(test_falls_back_for_empty_name_and_bio);
   RUN_TEST(test_limits_number_of_profiles);
   RUN_TEST(test_retry_backoff_is_bounded);
+  RUN_TEST(test_pagination_stops_and_respects_limit);
+  RUN_TEST(test_carousel_wraps_in_both_directions);
+  RUN_TEST(test_touch_mapping_clamps_and_inverts);
+  RUN_TEST(test_cooldown_handles_millis_rollover);
   UNITY_END();
 }
 
